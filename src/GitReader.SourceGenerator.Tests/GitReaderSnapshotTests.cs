502434849a87becbfe7700cf0673b6354fcaf801
Jan Trejbal <jan@trejbal.land>
2023-11-18 19:03:56 +0100

    Add TagRepositoryWorks, NoTagRepositoryWorks

--- src/GitReader.SourceGenerator.Tests/GitReaderSnapshotTests.cs
+++ src/GitReader.SourceGenerator.Tests/GitReaderSnapshotTests.cs
@@ -17,6 +17,29 @@ public class GitReaderSnapshotTests
         {
             ["build_property.GitInfo_RootDirectory"] = "v-tag-repository".GetRepositoryPath(),
             ["build_property.GitInfo_Namespace"] = "ApplicationNamespace",
+            ["build_property.GitInfo_UseCache"] = "false",
+        })
+    );
+
+    [Fact]
+    public Task TagRepositoryWorks() => TestHelper.Verify<GitInfoGenerator>(
+        new DateTimeOffset(2023, 11, 18, 0, 1, 2, TimeSpan.Zero),
+        new DictionaryAnalyzerConfigOptionsProvider(globalOptions: new Dictionary<string, string>
+        {
+            ["build_property.GitInfo_RootDirectory"] = "tag-repository".GetRepositoryPath(),
+            ["build_property.GitInfo_Namespace"] = "ApplicationNamespace",
+            ["build_property.GitInfo_UseCache"] = "false",
+        })
+    );
+
+    [Fact]
+    public Task NoTagRepositoryWorks() => TestHelper.Verify<GitInfoGenerator>(
+        new DateTimeOffset(2023, 11, 18, 0, 1, 2, TimeSpan.Zero),
+        new DictionaryAnalyzerConfigOptionsProvider(globalOptions: new Dictionary<string, string>
+        {
+            ["build_property.GitInfo_RootDirectory"] = "no-tag-repository".GetRepositoryPath(),
+            ["build_property.GitInfo_Namespace"] = "ApplicationNamespace",
+            ["build_property.GitInfo_UseCache"] = "false",
         })
     );
 }
